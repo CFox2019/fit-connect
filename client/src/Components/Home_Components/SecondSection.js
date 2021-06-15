34d3bff397eb5dd7cea7c088d0c38a0be0421fb5
Courtney Fox <courtney.fox@me.com>
2021-06-15 14:12:31 -0400

    added semicolons for best practice

--- client/src/Components/Home_Components/SecondSection.js
+++ client/src/Components/Home_Components/SecondSection.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Row, Container, Col, CardDeck, Card, Button } from 'react-bootstrap'
-import "./SecondSection.css"
+import React from 'react';
+import { Row, Container, Col, CardDeck, Card, Button } from 'react-bootstrap';
+import "./SecondSection.css";
 
 
 export const SecondSection = () => (
